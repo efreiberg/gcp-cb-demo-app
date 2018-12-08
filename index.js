64580d6fad8bbf39c2d18c0196c11176d71617c2
Eric Freiberg <eric.freiberg@pointsource.com>
2018-12-08 14:35:16 -0600

    Change deployment name

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var server = http.createServer((req, res) => {
     console.log(`Request From ${req.socket.remoteAddress}:${req.socket.remotePort}`);
     res.setHeader('Content-Type', 'application/json');
     res.statusCode = 200;
-    res.end(JSON.stringify({"status": `hello from ${req.socket.localAddress}:${req.socket.localPort}`}));
+    res.end(JSON.stringify({"status": `hello from ${req.socket.localAddress}:${req.socket.localPort} v1`}));
 });
 
 server.listen(PORT, HOST);
